79a26c2eec40011e1f516d35132930a25704e02b
John Lavery <121765789+jlavery249@users.noreply.github.com>
2023-01-02 10:24:49 -0500

    Create Linuxutilsinstall.sh

---
+++ MacOS/Linuxutilsinstall.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+brew install binutils
+brew install diffutils
+brew install ed
+brew install findutils 
+brew install gawk
+brew install gnu-indent 
+brew install gnu-sed 
+brew install gnu-tar 
+brew install gnu-which 
+brew install gnutls
+brew install grep 
+brew install gzip
+brew install screen
+brew install watch
+brew install wdiff
+brew install wget
+brew install bash
+brew install emacs
+brew install gdb  # gdb requires further actions to make it work. See `brew info gdb`.
+brew install gpatch
+brew install less
+brew install m4
+brew install make
+brew install nano
+brew install file-formula
+brew install git
+brew install openssh
+brew install perl
+brew install python
+brew install rsync
+brew install svn
+brew install unzip
+brew install vim
+brew install macvim
+brew install zsh
