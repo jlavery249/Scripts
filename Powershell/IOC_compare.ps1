8cd2665e31dad636002e34f1d5d12d1c86a21db3
John Lavery <121765789+jlavery249@users.noreply.github.com>
2023-01-02 09:56:36 -0500

    Rearranged for neatness

--- Powershell/IOC_compare.ps1
+++ Powershell/IOC_compare.ps1
@@ -1,16 +1,26 @@
-$a= 1
+$a = 1
 $ipiocs = Get-Content ips.txt
 $fileiocs = Get-Content files.txt
 $regiocs = get-content reg.txt
 
 while ($a -le 255)
 {
-    if(Test-Connection -Count 1 -Quiet 10.10.10.$a) {
-    $ip = 10.10.10.$a
-set-item WSMan:\localhost\Client\TrustedHosts -Value "$ip" -Concatenate
-    Invoke-command -ComputerName $ip -ScriptBlock { $using:fileiocs | ForEach-Object { $x = null; $x = Get-ChildItem $_; if( $x.Length -ne 0) { $x } }  #using uses a local variable on a remote box
+    if (Test-Connection -Count 1 -Quiet 10.10.10.$a)
+    {
+        $ip = 10.10.10.$a
+        set-item WSMan:\localhost\Client\TrustedHosts -Value "$ip" -Concatenate
+        Invoke-command -ComputerName $ip -ScriptBlock {
+            $using:fileiocs | ForEach-Object {
+                $x = null
+                $x = Get-ChildItem $_
+                if ($x.Length -ne 0) { $x }
+            }
+        }
     }
-    $a++;
+    $a++
 }
 
-    Get-item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run | % { $reg = $_.Property; $using:regicos | ? { $reg -eq $_ } }
+Get-item HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run | % {
+    $reg = $_.Property
+    $using:regicos | ? { $reg -eq $_ }
+}
