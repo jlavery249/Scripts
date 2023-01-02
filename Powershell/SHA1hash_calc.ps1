bc2bd968a311e31785ab3bab929d0e1dc9741bb0
John Lavery <121765789+jlavery249@users.noreply.github.com>
2023-01-01 22:42:43 -0500

    Create SHA1hash_calc.ps1

---
+++ Powershell/SHA1hash_calc.ps1
@@ -0,0 +1,1 @@
+Get-ChildItem 'C:\users\DCI Student\Documents\exercise_8' -Recurse | % { Get-Item $_.FullName -Stream * } | where stream -ne ':$Data' | % { Get-filehash -algorithm SHA1 $_.filename } | select hash, path | format-list
