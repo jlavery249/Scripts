a837704bf4f384c9bd70079567a3afdfb6193789
John Lavery <121765789+jlavery249@users.noreply.github.com>
2023-01-02 09:54:30 -0500

    Removed comments

--- Powershell/IOC_compare.ps1
+++ Powershell/IOC_compare.ps1
@@ -9,12 +9,6 @@ while ($a -le 255)
     $ip = 10.10.10.$a
 set-item WSMan:\localhost\Client\TrustedHosts -Value "$ip" -Concatenate
     Invoke-command -ComputerName $ip -ScriptBlock { $using:fileiocs | ForEach-Object { $x = null; $x = Get-ChildItem $_; if( $x.Length -ne 0) { $x } }  #using uses a local variable on a remote box
-    <# Alternativley you can copy the file over 
-    
-    $mysession = New-PSSession $ip
-    Copy-Item -Path "C:\Users\DCI Student\Desktop\fileiocs.txt" - Destination "C:\user\dci student\desktop" - ToSession $mysession---- this is from local to remote
-    or
-    Copy-Item -Path "C:\Users\DCI Student\Desktop\fileiocs.txt" - Destination "C:\user\dci student\desktop" - FromSession $mysession----- remote to local#>
     }
     $a++;
 }
