d2131235025726cba4015c9e41c1c3ea017e999b
John Lavery <121765789+jlavery249@users.noreply.github.com>
2023-01-01 22:48:06 -0500

    Added header and description

--- Powershell/SHA1hash_calc.ps1
+++ Powershell/SHA1hash_calc.ps1
@@ -1,1 +1,12 @@
+#############################################################################################
+#  SHA1 Hash calculator                                                                     #
+#  Created by John Lavery                                                                   #
+#  Year of 2018                                                                             #
+#  Written in Powershell                                                                    #
+#  Description:                                                                             #
+#   This oneliner searches for all files in the specified directory and its subdirectories  #
+#   and calculates the SHA1 hash for each file. The hash and path for each file are then    #
+#   displayed in a list.                                                                    #
+#############################################################################################
+
 Get-ChildItem 'C:\users\DCI Student\Documents\exercise_8' -Recurse | % { Get-Item $_.FullName -Stream * } | where stream -ne ':$Data' | % { Get-filehash -algorithm SHA1 $_.filename } | select hash, path | format-list
