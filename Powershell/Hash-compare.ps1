18c30e76644982aa2b6419a6076cde1707084009
John Lavery <121765789+jlavery249@users.noreply.github.com>
2023-01-02 10:57:52 -0500

    Update Hash-compare.ps1
    Added comments and header

--- Powershell/Hash-compare.ps1
+++ Powershell/Hash-compare.ps1
@@ -1,3 +1,15 @@
+
+############################################################################################################
+#  Hash Compare                                                                                            #
+#  Created by John Lavery                                                                                  #
+#  Year of 2022                                                                                            #
+#  Written in Powershell                                                                                   #
+#  Description:                                                                                            #
+#  Gathers a systems file hashes and then compares them to a list of file hashes that are specified. This  #
+#  script is helpful specifically if you are looking for bad files by file system hash integrity           #
+############################################################################################################
+
+## Declaring functions
 Function IOCHash-Compare
 {
     select-string -Pattern (gc $fin) -Path C:\FilesHash.csv
@@ -9,6 +21,8 @@ Function Compilesystem-Hashes
         Get-FileHash -ErrorAction SilentlyContinue |
         Export-Csv -Path C:\FilesHash.csv -NoTypeInformation
 }
+
+## user prompt and run through declared functions
 
 $fin = read-host -Prompt "Hash List File Location"
 write-host "Gathering system file hashes now, This may take a while..." 
